23596067aad931fee93f641cf9c835eec52c7647
Katakuranatsumi <usako7233@gmail.com>
2018-12-21 17:00:21 +0900

    Commentのmodel作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     
-  resources :posts
-    
+  resources :posts do
+     resources :comments    
+  end    
 #  サーバーを立ち上げた最初の画面をindexにする処理
   root 'posts#index'    
     
